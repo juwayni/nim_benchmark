5161cebdc78e2feafcfcd1a213ac0cf7b164fa44
juwayni <ghazalialshafi@outlook.com>
2026-03-02 07:56:02 +0530

    Add files via upload

---
+++ run.js
@@ -0,0 +1,280 @@
+{
+  "Binarytrees::Obj": {
+    "checksum": 100663248,
+    "depth": 20,
+    "iterations": 40
+  },
+  "Binarytrees::Arena": {
+    "checksum": 100663248,
+    "depth": 20,
+    "iterations": 40
+  },
+  "Brainfuck::Array": {
+    "checksum": 954437102,
+    "program": ">++[<+++++++++++++>-]<[[>+>+<<-]>[<+>-]++++++++[>++++++++<-]>.[-]<<>++++++++++[>++++++++++[>++++++++++[>++++++++++[>++++++++++[>++++++++++[>++++++++++[-]<-]<-]<-]<-]<-]<-]<-]++++++++++.",
+    "warmup_program": "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.",
+    "warmup_iterations": 1000,
+    "iterations": 1
+  },
+  "Brainfuck::Recursion": {
+    "checksum": 954437102,
+    "program": ">++[<+++++++++++++>-]<[[>+>+<<-]>[<+>-]++++++++[>++++++++<-]>.[-]<<>++++++++++[>++++++++++[>++++++++++[>++++++++++[>++++++++++[>++++++++++[>++++++++++[-]<-]<-]<-]<-]<-]<-]<-]++++++++++.",
+    "warmup_program": "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.",
+    "warmup_iterations": 1000,
+    "iterations": 1
+  },
+  "Matmul::Single": {
+    "checksum": 3655420808,
+    "n": 900,
+    "iterations": 10
+  },
+  "Matmul::T4": {
+    "checksum": 3655420808,
+    "n": 900,
+    "iterations": 10
+  },
+  "Matmul::T8": {
+    "checksum": 3655420808,
+    "n": 900,
+    "iterations": 10
+  },
+  "Matmul::T16": {
+    "checksum": 3655420808,
+    "n": 900,
+    "iterations": 10
+  },
+  "Base64::Encode": {
+    "checksum": 3516658482,
+    "size": 500000,
+    "iterations": 5000
+  },
+  "Base64::Decode": {
+    "checksum": 1329134018,
+    "size": 500000,
+    "iterations": 4000
+  },
+  "Json::Generate": {
+    "checksum": 120,
+    "coords": 20000,
+    "iterations": 100
+  },
+  "Json::ParseDom": {
+    "checksum": 3989922952,
+    "coords": 10000,
+    "iterations": 100
+  },
+  "Json::ParseMapping": {
+    "checksum": 3826684672,
+    "coords": 25000,
+    "iterations": 100
+  },
+  "Etc::Sieve": {
+    "checksum": 1042248060,
+    "limit": 3000000,
+    "iterations": 270
+  },
+  "Etc::TextRaytracer": {
+    "checksum": 2761140480,
+    "w": 800,
+    "h": 800,
+    "iterations": 100
+  },
+  "Etc::NeuralNet": {
+    "checksum": 144809143,
+    "iterations": 2200
+  },
+  "Etc::CacheSimulation": {
+    "checksum": 3263751556,
+    "values": 5000,
+    "size": 2500,
+    "iterations": 5000
+  },
+  "Etc::GameOfLife": {
+    "checksum": 3805797673,
+    "w": 500,
+    "h": 500,
+    "iterations": 500
+  },
+  "Etc::Words": {
+    "checksum": 1998586832,
+    "words": 50000,
+    "word_len": 4,
+    "iterations": 360
+  },
+  "Etc::LogParser": {
+    "checksum": 2265912,
+    "iterations": 20,
+    "lines_count": 45000
+  },
+  "Sort::Quick": {
+    "checksum": 118065786,
+    "size": 150000,
+    "iterations": 100
+  },
+  "Sort::Merge": {
+    "checksum": 118065786,
+    "size": 150000,
+    "iterations": 100
+  },
+  "Sort::Self": {
+    "checksum": 118065786,
+    "size": 150000,
+    "iterations": 100
+  },
+  "Graph::BFS": {
+    "checksum": 50400,
+    "vertices": 500000,
+    "jumps": 20,
+    "jump_len": 1000,
+    "iterations": 40
+  },
+  "Graph::DFS": {
+    "checksum": 22194,
+    "vertices": 500000,
+    "jumps": 20,
+    "jump_len": 1000,
+    "iterations": 15
+  },
+  "Graph::AStar": {
+    "checksum": 1406400,
+    "vertices": 500000,
+    "jumps": 20,
+    "jump_len": 1000,
+    "iterations": 1000
+  },
+  "Hash::SHA256": {
+    "checksum": 2844672256,
+    "size": 1000000,
+    "iterations": 1200
+  },
+  "Hash::CRC32": {
+    "checksum": 2318723244,
+    "size": 1000000,
+    "iterations": 150
+  },
+  "Calculator::Ast": {
+    "checksum": 1670614456,
+    "operations": 11000,
+    "iterations": 100
+  },
+  "Calculator::Interpreter": {
+    "checksum": 38749936,
+    "operations": 35000,
+    "iterations": 100
+  },
+  "Maze::Generator": {
+    "checksum": 3738188563,
+    "w": 1400,
+    "h": 1400,
+    "iterations": 10
+  },
+  "Maze::BFS": {
+    "checksum": 2906799,
+    "w": 1400,
+    "h": 1400,
+    "iterations": 70
+  },
+  "Maze::AStar": {
+    "checksum": 2906799,
+    "w": 1400,
+    "h": 1400,
+    "iterations": 70
+  },
+  "CLBG::Pidigits": {
+    "checksum": 3205646197,
+    "amount": 1000,
+    "iterations": 120
+  },
+  "CLBG::Fannkuchredux": {
+    "checksum": 135762480,
+    "n": 8,
+    "iterations": 700
+  },
+  "CLBG::Fasta": {
+    "checksum": 2553600673,
+    "n": 85000,
+    "iterations": 100
+  },
+  "CLBG::Knuckeotide": {
+    "checksum": 3141068829,
+    "n": 6000,
+    "iterations": 100
+  },
+  "CLBG::RegexDna": {
+    "checksum": 1111191109,
+    "n": 20000,
+    "iterations": 200
+  },
+  "CLBG::Revcomp": {
+    "checksum": 1125863152,
+    "n": 100000,
+    "iterations": 500
+  },
+  "CLBG::Mandelbrot": {
+    "checksum": 3455988829,
+    "w": 400,
+    "h": 400,
+    "iterations": 100
+  },
+  "CLBG::Nbody": {
+    "checksum": 338799744,
+    "iterations": 20000
+  },
+  "CLBG::Spectralnorm": {
+    "checksum": 1052198219,
+    "size": 1200,
+    "iterations": 132
+  },
+  "Compress::BWTEncode": {
+    "checksum": 3600000,
+    "size": 100000,
+    "iterations": 30
+  },
+  "Compress::BWTDecode": {
+    "checksum": 216100216,
+    "size": 1000001,
+    "iterations": 180
+  },
+  "Compress::HuffEncode": {
+    "checksum": 62727600,
+    "size": 1000002,
+    "iterations": 200
+  },
+  "Compress::HuffDecode": {
+    "checksum": 420101260,
+    "size": 1000003,
+    "iterations": 350
+  },
+  "Compress::ArithEncode": {
+    "checksum": 15303600,
+    "size": 100004,
+    "iterations": 500
+  },
+  "Compress::ArithDecode": {
+    "checksum": 26501320,
+    "size": 100005,
+    "iterations": 220
+  },
+  "Compress::LZWEncode": {
+    "checksum": 500880,
+    "size": 200006,
+    "iterations": 100
+  },
+  "Compress::LZWDecode": {
+    "checksum": 1920113440,
+    "size": 1000007,
+    "iterations": 1600
+  },
+  "Distance::Jaro": {
+    "checksum": 567348912,
+    "iterations": 780,
+    "count": 1000,
+    "size": 50
+  },
+  "Distance::NGram": {
+    "checksum": 397440,
+    "iterations": 100,
+    "count": 10,
+    "size": 20000
+  }
+}
